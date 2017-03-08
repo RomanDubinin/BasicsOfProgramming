d2267aab1f5e72f5b0afff3a02eddcf5ebf3981e
romandubinin <romandubinin94@gmail.com>
2017-03-08 11:23:16 +0500

    генератор тасков

---
+++ TaskSchedule/TaskGenerator/NormallyDistributedRandomNumberGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace TaskGenerator
+{
+    public class NormallyDistributedRandomNumberGenerator
+    {
+        private Random random;
+        private double previous = Double.NaN;
+
+        public NormallyDistributedRandomNumberGenerator(Random random)
+        {
+            this.random = random;
+        }
+
+        public double GetNextDouble()
+        {
+            if (!double.IsNaN(previous))
+            {
+                var res = previous;
+                previous = Double.NaN;
+                return res;
+            }
+
+            double x, y, s;
+            do
+            {
+                x = 2 * random.NextDouble() - 1;
+                y = 2 * random.NextDouble() - 1;
+
+                s = x * x + y * y;
+            } while (x <= -1 || y <= -1 || s >= 1 || s <= double.Epsilon);
+
+            var r = Math.Sqrt(-2 * Math.Log(s) / s);
+            previous = r * x;
+            return r * y;
+        }
+
+        public double GetNextDouble(double expectation, double dispersion)
+        {
+            return (GetNextDouble() * dispersion) + expectation;
+        }
+    }
+}
---
+++ TaskSchedule/TaskGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TaskGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksCount = 20;
+            var latestStart = 10;
+            var expectationOfDuration = 6;
+            var dispersionOfDuration = 2;
+
+            var normalRandom = new NormallyDistributedRandomNumberGenerator(new Random());
+            var uniformRandom = new Random();
+
+            var tasks = Enumerable
+                .Range(0, tasksCount)
+                .Select(x => new
+                {
+                    start = uniformRandom.Next(latestStart),
+                    duration = (int) normalRandom.GetNextDouble(expectationOfDuration, dispersionOfDuration)
+                })
+                .ToArray();
+
+            File.WriteAllText("tasks.txt", JsonConvert.SerializeObject(tasks, Formatting.Indented));
+        }
+
+        static void Sample()
+        {
+            var numOfSamples = 1000;
+            var random = new NormallyDistributedRandomNumberGenerator(new Random());
+            var values = Enumerable
+                .Range(0, numOfSamples)
+                .Select(x => random.GetNextDouble(100, 20))
+                .GroupBy(x => (int)((int)x / 10) * 10)
+                .OrderBy(x => x.Key)
+                .ToList();
+
+
+            foreach (var value in values)
+            {
+                Console.WriteLine(value.Key + " " + value.Count());
+            }
+        }
+    }
+}
